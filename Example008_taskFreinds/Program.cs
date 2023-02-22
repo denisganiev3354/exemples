28e5ab2d8351bae0bc9f844d6188561db0fd3450
denisganiev3354 <denisganiev3354@gmail.com>
2023-02-22 21:35:00 +0400

    Первая рабочая версия

---
+++ Example008_taskFreinds/Program.cs
@@ -0,0 +1,10 @@
+﻿int distance =10000;
+int firstFriendSpeed = 1;
+int dogSpeed = 5;
+int Time = 0;
+if ( distance > 10 )
+{
+    Console.WriteLine ("Время пройденое собакой");
+    Console.Write ( Time = distance / ( firstFriendSpeed + dogSpeed ));
+    Console.Write (" минут");
+}
