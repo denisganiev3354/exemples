a85295fb9367b9bdbc4b6956683d6f3159537bad
denisganiev3354 <denisganiev3354@gmail.com>
2023-02-22 22:42:30 +0400

    Полная программа с ошибкой оператора 2

--- Example008_taskFreinds/Program.cs
+++ Example008_taskFreinds/Program.cs
@@ -1,10 +1,28 @@
-﻿int distance =10000;
+﻿int count = 0;
+int distance =10000;
 int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
 int dogSpeed = 5;
+int Friend = 0;
 int Time = 0;
-if ( distance > 10 )
-{
-    Console.WriteLine ("Время пройденое собакой");
-    Console.Write ( Time = distance / ( firstFriendSpeed + dogSpeed ));
-    Console.Write (" минут");
-}
+
+while ( distance > 10 );
+
+    while (Friend == 1);
+    {
+        Time = distance / ( firstFriendSpeed + dogSpeed );
+        Friend = 2;
+        distance = distance - ( firstFriendSpeed + secondFriendSpeed ) * Time;
+        count++;
+    }
+     while (Friend == 2);
+    {
+        Time = distance /( secondFriendSpeed + dogSpeed );
+        Friend = 1;
+        distance = distance - ( firstFriendSpeed + secondFriendSpeed ) * Time;
+        count++;
+    }
+    
+Console.Write("Собака прошла ");
+Console.Write(count);
+Console.WriteLine(" раз");
