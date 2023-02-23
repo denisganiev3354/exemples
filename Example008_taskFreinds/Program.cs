433b773cdf5d50b3bcf4aeb0c33ca84a18d7c531
denisganiev3354 <denisganiev3354@gmail.com>
2023-02-23 19:04:00 +0400

    программа работает, но выдает не верный ответ

--- Example008_taskFreinds/Program.cs
+++ Example008_taskFreinds/Program.cs
@@ -1,28 +1,30 @@
 ﻿int count = 0;
-int distance =10000;
+int distance =100;
 int firstFriendSpeed = 1;
 int secondFriendSpeed = 2;
 int dogSpeed = 5;
-int Friend = 0;
+int Friend = 2;
 int Time = 0;
 
-while ( distance > 10 );
-
-    while (Friend == 1);
+while ( distance > 10 )
+{
+    if ( Friend == 1)
     {
         Time = distance / ( firstFriendSpeed + dogSpeed );
         Friend = 2;
         distance = distance - ( firstFriendSpeed + secondFriendSpeed ) * Time;
         count++;
     }
-     while (Friend == 2);
+    if ( Friend == 2)
     {
         Time = distance /( secondFriendSpeed + dogSpeed );
         Friend = 1;
         distance = distance - ( firstFriendSpeed + secondFriendSpeed ) * Time;
         count++;
+        
     }
-    
-Console.Write("Собака прошла ");
-Console.Write(count);
-Console.WriteLine(" раз");
+    Console.Write("Собака прошла ");
+    Console.Write(count);
+    Console.Write(" раз(а)");
+
+}
