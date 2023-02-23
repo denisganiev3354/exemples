b2ab68d9a2b44801af435827913221287e83017c
denisganiev3354 <denisganiev3354@gmail.com>
2023-02-23 19:29:29 +0400

    Рабочая программа, но в результате, порядок ответов.

--- Example008_taskFreinds/Program.cs
+++ Example008_taskFreinds/Program.cs
@@ -1,5 +1,5 @@
 ﻿int count = 0;
-int distance =100;
+int distance =10000;
 int firstFriendSpeed = 1;
 int secondFriendSpeed = 2;
 int dogSpeed = 5;
@@ -25,6 +25,6 @@ while ( distance > 10 )
     }
     Console.Write("Собака прошла ");
     Console.Write(count);
-    Console.Write(" раз(а)");
-
+    Console.WriteLine(" раз(а). ");
+    
 }
